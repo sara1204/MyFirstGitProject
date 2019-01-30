e0dafd3ef20f2b8d58e4df176ca7a5063d687f5c
sara1204 <knezevicsara@gmx.at>
2019-01-30 17:23:59 +0100

    add Unity - Datei

---
+++ Unity/Assets/Scripts/TestObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : MonoBehaviour {
+    
+    string helloworld ="HelloWorld";
+    
+    public void PrintHelloWorld() 
+    {
+        Debug.Log(helloworld);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
